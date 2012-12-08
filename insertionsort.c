48cf8f402761ef76b788166052710906e264a9e2
Sunny <sny@sdbyte.com>
2012-12-08 08:42:21 +0530

    insertionsort randomization added
    added functionality of allowing entering both maunal sequence as well
    as random sequence generated by snyrandom function from random.c

--- insertionsort.c
+++ insertionsort.c
@@ -1,6 +1,8 @@
 #define MAX 10000
 #include <stdio.h>
+#include "random.c"
 
+void output();
 //Global Definations
 int a[MAX]={0};
 int n=0;
@@ -9,15 +11,35 @@ int n=0;
 void enter()
 {
 	int i;
+	int choice=0;
 	
 	printf("Enter array size:");
 	scanf("%d",&n);
+	
+	printf("Do you want to enter sequence or randomize sequence? Enter 1 for manual input and 2 for randomization:");
+	scanf("%d",&choice);
 
-	printf("\n\nNow enter input number sequence:");
-
-	for(i=0;i<n;i++)
+	if(choice == 1)
 	{
-		scanf("%d",&a[i]);
+		printf("\n\nNow enter input number sequence:");
+
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&a[i]);
+		}
+	}
+
+	else if(choice == 2)
+	{
+		for(i=0;i<n;i++)
+		{
+			a[i]=snyrandom();
+			snyrand=a[i];
+		}
+
+		printf("\n\nRandomized array is :");
+	
+		output();
 	}
 
 }
